23288329611de270d48f1c73814667ab8ae48e13
Vincent Seyller <vseyller@laposte.net>
2022-01-25 10:22:06 +0100

    Ajout pages

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,9 +1,23 @@
-document.addEventListener('deviceready', onDeviceReady, false);
-document.getElementById('deviceready').classList.add('ready');
-    
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+document.getElementById("start").addEventListener("click", handleCreateSession);
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+let storage = window.localStorage;
+
+function handleCreateSession() {
+    let value = storage.getItem("id");
+    if(value == null) {
+        let newId = generateId(40);
+        storage.setItem("id", newId);
+    }
+    console.log(storage.getItem("id"));
+    window.location.href = "./questions.html";
+};
+
+function generateId(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+   return result;
 }
--- app/www/js/questions.js
+++ app/www/js/questions.js
@@ -1,11 +1,15 @@
-var responseButtons = document.getElementsByClassName('response');
-for(var button of responseButtons) {
-    button.addEventListener('click', handleResponse);
+let responseButtons = document.getElementsByClassName("response");
+for(let button of responseButtons) {
+    button.addEventListener("click", handleResponse);
 }
+document.addEventListener("backbutton", handleBackButton, false);
+document.getElementById("score-button").addEventListener("click", handleScoreButton);
 
-let images = ["img/1.png", "img/2.png", "img/3.png"]
+let images = ["img/house.png", "img/heating.png", "img/faucets.png"]
 
 let questions = ["Question 1", "Question 2", "Question 3"];
+
+let categories = ["House", "Holidays", "Food"];
 
 let reponses = [
     ["Réponse 1.1", "Réponse 1.2", "Réponse 1.3"],
@@ -13,13 +17,47 @@ let reponses = [
     ["Réponse 3.1", "Réponse 3.2", "Réponse 3.3"]
 ];
 
+let storage = window.localStorage;
+
+let id;
+let nbQuestions;
+
+handleGetSession();
+
+handleVisibility();
+
 getQuestion();
 
+function handleGetSession() {
+    id = storage.getItem("id");
+    nbQuestions = storage.getItem("nbQuestions");
+    if(nbQuestions == "NaN") {
+        nbQuestions = 0;
+        storage.setItem("nbQuestions", nbQuestions);
+    }
+};
+
 function getQuestion() {
-    var rd = Math.floor(Math.random() * 3);
-    document.getElementById('question').innerHTML = questions[rd];
-    var responseButtons = document.getElementsByClassName('response');
-    for(var button in responseButtons) {
+    let rd = Math.floor(Math.random() * 3);
+    document.getElementById("question").innerHTML = questions[rd];
+    let categorie = document.getElementById("categorie");
+    categorie.innerHTML = categories[rd];
+    switch (categories[rd]) {
+        case "House":
+            categorie.style.backgroundColor = "green";
+            break;
+        case "Holidays":
+            categorie.style.backgroundColor = "blue";
+            break;
+        case "Food":
+            categorie.style.backgroundColor = "red";
+            break;
+        default:
+            categorie.style.backgroundColor = "white";
+            break;
+    }
+    let responseButtons = document.getElementsByClassName("response");
+    for(let button in responseButtons) {
         responseButtons[button].innerHTML = reponses[rd][button];
     }
     document.getElementById("image").src = images[rd];
@@ -27,5 +65,28 @@ function getQuestion() {
 
 function handleResponse(e) {
     console.log("Réponse: " + e.target.id);
+    nbQuestions++;
+    storage.setItem("nbQuestions", nbQuestions);
+    handleVisibility();
     getQuestion();
+}
+
+function handleVisibility() {
+    let noAccessTip = document.getElementById("no-access");
+    let scoreButton = document.getElementById("score-button");
+    if(nbQuestions >= 10) {
+        noAccessTip.style.visibility = "hidden";
+        scoreButton.disabled = false;
+    } else {
+        noAccessTip.style.visibility = "visible";
+        scoreButton.disabled = true;
+    }
+}
+
+function handleBackButton() {
+    window.location.href = "./questions.html";
+}
+
+function handleScoreButton() {
+    window.location.href = "./score.html";
 }
