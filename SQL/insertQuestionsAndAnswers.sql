c6cdde2cbaefd8cb5548f5f45b13139943cf6d3e
DLTBryan <bryan.dallest@gmail.com>
2022-01-25 09:15:44 +0100

    Added script only for inserting questions and answers

--- SQL/insertQuestionsAndAnswers.sql
+++ SQL/insertQuestionsAndAnswers.sql
@@ -1,3 +1,7 @@
 
 INSERT INTO `smartcarbon`.`Question` (`content`, `valuePoint`, `image_idimage`)
 VALUES("What type of housing do you live in?", 20, "firstImage");
+
+
+INSERT INTO `smartcarbon`.`Question` (`content`, `valuePoint`, `image_idimage`)
+VALUES("What type of housing do you live in?", 20, "firstImage");
